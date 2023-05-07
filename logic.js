9d5517528d4830ff01270601cd3fea8db91c24c9
BryceNesius <bnesius@hotmail.com>
2023-05-06 20:15:05 -0400

    Initial push with all content.

---
+++ logic.js
@@ -0,0 +1,36 @@
+function showFrontEnd() {
+    var x = document.getElementById("machine-learning");
+    var y = document.getElementById('front-end');
+    var z = document.getElementById('cybersecurity');
+
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
+  }
+function showML() {
+var x = document.getElementById("machine-learning");
+var y = document.getElementById('front-end');
+var z = document.getElementById('cybersecurity');
+
+x.style.display = "block";
+y.style.display = "none";
+z.style.display = "none";
+}
+function showCyberSec() {
+    var x = document.getElementById("machine-learning");
+    var y = document.getElementById('front-end');
+    var z = document.getElementById('cybersecurity');
+    
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "block";
+    }
+function showAll() {
+    var x = document.getElementById("machine-learning");
+    var y = document.getElementById('front-end');
+    var z = document.getElementById('cybersecurity');
+    
+    x.style.display = "block";
+    y.style.display = "block";
+    z.style.display = "block";
+    }
